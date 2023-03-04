de0a0da506963d66265ef04e79c0c4d8e811d4e1
gleb-erokhin <gleb.erokhin@gmail.com>
2023-03-04 19:49:32 +0300

    initial commit four tasks

---
+++ HW1/simple_calc.java
@@ -0,0 +1,8 @@
+package HW.HW1;
+
+/*
+ Реализовать простой калькулятор
+ */
+public class simple_calc {
+
+}
---
+++ HW1/simple_num.java
@@ -0,0 +1,8 @@
+package HW.HW1;
+
+/*
+ * Вывести все простые числа от 1 до 1000
+ */
+public class simple_num {
+
+}
