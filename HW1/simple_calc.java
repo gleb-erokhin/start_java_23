95f0cc183575e48f0f718ede5648da193a2aad8f
gleb-erokhin <gleb.erokhin@gmail.com>
2023-03-07 21:41:55 +0300

    simple_calc.java is done

--- HW1/simple_calc.java
+++ HW1/simple_calc.java
@@ -6,33 +6,45 @@ import java.util.Scanner;
  Реализовать простой калькулятор
  */
 public class simple_calc {
+    // "Метод ввода данных"
+    public static void inData(String[] args) {
+        Scanner scanner = new Scanner(System.in); // объявляем объекта класса сканер для считывания данных
+        System.out.print("enter the number one: ");
+        int number_a = scanner.nextInt(); // Полученное выражение записываем в переменную 1
+        System.out.print("enter the number two: ");
+        int number_b = scanner.nextInt(); // Полученное выражение записываем в переменную 2
+        System.out.print("enter the operation + - / *: ");
+        String operation = scanner.next(); // Получаем знак выполнения операции
+        scanner.close(); // закрываем объект сканер для экономии памяти
+        // передаем данные в медод вычисления, после чего выводим ответ
+        System.out.println(calc(operation, number_a, number_b));
+    }
+
+    // Создаем медод для калькулятора, принимает на вход "метод ввода данных", и два
+    // числа
     public static int calc(String operation, int number_a, int number_b) {
-        int calc = 0;
+        int calc = 0; // объявляем переменную для конечного результата
+        // задаем выбор операция для вычисления
         switch (operation) {
             case "+":
                 calc = number_a + number_b;
-                return calc;
+                break;
             case "-":
                 calc = number_a - number_b;
-                return calc;
+                break;
             case "/":
                 calc = number_a / number_b;
-                return calc;
+                break;
             case "*":
                 calc = number_a * number_b;
-                return calc;
+                break;
+            default:
+                System.out.println("Неверная операция, попробуйте снова");
         }
+        return calc;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("enter the number one: ");
-        int number_a = scanner.nextInt();
-        System.out.print("enter the number two: ");
-        int number_b = scanner.nextInt();
-        System.out.print("enter the operation + - / *: ");
-        String operation = scanner.next();
-        scanner.close();
-        calc(operation, number_a, number_b);
+        inData(args); // вызываем метод ввода данных
     }
 }
