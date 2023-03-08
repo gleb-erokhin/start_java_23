6cab8bb7d5aac55cc0455e1ef276e88f721b9da4
gleb-erokhin <gleb.erokhin@gmail.com>
2023-03-08 16:39:32 +0300

    equality_num.java is done

--- HW1/equality_num.java
+++ HW1/equality_num.java
@@ -1,5 +1,7 @@
 package HW.HW1;
 
+// import java.util.Scanner;
+
 /* 
 *+Задано уравнение вида q + w = e, q, w, e >= 0. 
 Некоторые цифры могут быть заменены знаком вопроса, 
@@ -8,5 +10,19 @@ package HW.HW1;
 Предложить хотя бы одно решение или сообщить, что его нет.
 */
 public class equality_num {
-
+    public static void main(String[] args) {
+        System.out.println("Найти цифру под знаком ? в уравнении -> 2? + ?5 = 69");
+        for (int i = 0; i < 9; i++) {
+            for (int j = i; j < 9; j++) {
+                int answer = (20 + i) + ((j * 10) + 5);
+                if (answer == 69) {
+                    if (i == j) {
+                        System.out.println("неизвестная цифра: ");
+                        System.out.println(i);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
