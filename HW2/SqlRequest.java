dcb586e9d79829b61fa4f0acc17787fd994268fa
gleb-erokhin <gleb.erokhin@gmail.com>
2023-03-10 07:00:28 +0300

    update gitignore and work on the json parser

--- HW2/SqlRequest.java
+++ HW2/SqlRequest.java
@@ -2,6 +2,13 @@ package HW.HW2;
 
 // import java.util.Arrays;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 /*
 Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. 
 Данные для фильтрации приведены ниже в виде json строки.
@@ -10,13 +17,34 @@ package HW.HW2;
 */
 public class SqlRequest {
     public static void main(String[] args) {
-        String test = "'name:Ivanov', 'country:Russia', 'city:Moscow', 'age:null'";
-        StringBuilder nameLine = new StringBuilder(test);
-        System.out.println(nameLine);
 
-        // String name = "";
-        // String country = "";
-        // String city = "";
-        // String age = "";
+        try(Object o = new JSONParser().parse(new FileReader("D:/4. Projects/7.java_prod/HW/HW2/file.json"))) {
+            JSONObject j = (JSONObject) o;
+            String name = (String) j.get("name");
+            String country = (String) j.get("Country");
+            System.out.println("name =" + name + "and" + "country =" + country);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+//        String test = "'name:Ivanov', 'country:Russia', 'city:Moscow', 'age:null'";
+//        // StringBuilder nameLine = new StringBuilder(test);
+//        System.out.println(test);
+//        String[] y = test.strip().split(",");
+//        System.out.println(y);
+//        for (String w : y) {
+//            System.out.println(w);
+//        }
+        // for (int i = 0; i < y.length; i++) {
+        // System.out.println(y[i]);
+
+        // }
     }
 }
+
+// String name = "";
+// String country = "";
+// String city = "";
+// String age = "";
